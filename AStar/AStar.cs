f37b91d04fef52c32e67d68d1df18a8375c716ea
bonum <bonum@DESKTOP-9GFEBCB>
2016-08-23 23:02:18 +0300

    optimistation

--- AStar/AStar.cs
+++ AStar/AStar.cs
@@ -6,30 +6,32 @@ using System.Linq;
 namespace AStar
 {
 
-    public class AStar
+    public static class AStar
     {
         private class Node : IComparable<Node>
         {
-            public Node Previous;
-            public Point Position;
-            public int G { get; private set; }
-            public float H { get; private set; }
-            public float F => G + H;
-            public int CompareTo(Node node) => F.CompareTo(node.F);
-            public override int GetHashCode() => Position.GetHashCode();
+            public readonly Node Previous;
+            public readonly Point Position;
+            public int G { get; }
+            private float F { get; }
+
             public Node(Node prev, Point pos, float h, int g)
             {
                 Previous = prev;
                 Position = pos;
-                H = h;
+                F = g + h;
                 G = g;
             }
+
+            public int CompareTo(Node node) => F.CompareTo(node.F);
+            public override int GetHashCode() => Position.GetHashCode();
         }
 
         private static IEnumerable<Point> SupposedNeighborsOf(Point current)
         {
             var x = current.X;
             var y = current.Y;
+
             yield return new Point(x + 1, y);
             yield return new Point(x - 1, y);
             yield return new Point(x, y + 1);
@@ -40,27 +42,31 @@ namespace AStar
             yield return new Point(x - 1, y + 1);
             yield return new Point(x + 1, y - 1);
         }
-        private static IEnumerable<Point> PassableNeighborsOf(Point current, bool[,] map)
+
+        private static IEnumerable<Point> PassableNeighborsOf(Point current, bool[,] obstaclesMap, Size mapSize)
         {
-            var mapSize = new Size(map.GetLength(0), map.GetLength(1));
             var ret = new List<Point>();
             foreach (var p in SupposedNeighborsOf(current))
             {
-                if (p.X < 0 || p.X >= mapSize.Width || p.Y < 0 || p.Y >= mapSize.Height || !map[p.X, p.Y]) continue;
+                if (p.X < 0 || p.X >= mapSize.Width || p.Y < 0 || p.Y >= mapSize.Height || !obstaclesMap[p.X, p.Y])
+                    continue;
                 ret.Add(p);
             }
             return ret;
         }
-        private static int NeighborOffset(Point current, Point neighbor) => current.X == neighbor.X || current.Y == neighbor.Y ? 10 : 14;
-        private static float H(Point from, Point to) => (float)Math.Sqrt(Math.Pow(to.X - from.X, 2) + Math.Pow(to.Y - from.Y, 2));
+
+        private static int NeighborOffset(Point current, Point neighbor)
+            => current.X == neighbor.X || current.Y == neighbor.Y ? 10 : 14;
+
+        private static float H(Point from, Point to)
+            => (float) Math.Sqrt(Math.Pow(to.X - from.X, 2) + Math.Pow(to.Y - from.Y, 2));
+
         public static List<Point> Search(Point start, Point end, bool[,] map)
         {
             var mapSize = new Size(map.GetLength(0), map.GetLength(1));
-            var open = new List<Node>();
-            var nodes = new Dictionary<Point, Node>();
             var startNode = new Node(null, start, H(start, end), 0);
-            open.Add(startNode);
-            nodes.Add(startNode.Position, startNode);
+            var open = new List<Node>() {startNode};
+            var nodes = new Dictionary<Point, Node> {{startNode.Position, startNode}};
             while (open.Any())
             {
                 open.Sort();
@@ -69,18 +75,19 @@ namespace AStar
                 {
                     return Result(current);
                 }
-                foreach (var p in PassableNeighborsOf(current.Position, map))
+                foreach (var p in PassableNeighborsOf(current.Position, map, mapSize))
                 {
                     if (nodes.ContainsKey(p)) continue;
-                    var node = new Node(current, p, H(current.Position, p), current.G + NeighborOffset(current.Position, p));
+                    var node = new Node(current, p, H(current.Position, p),
+                        current.G + NeighborOffset(current.Position, p));
                     nodes.Add(p, node);
                     open.Add(node);
                 }
                 open.Remove(current);
             }
             return null;
-
         }
+
         private static List<Point> Result(Node current)
         {
             var ret = new List<Point>();
